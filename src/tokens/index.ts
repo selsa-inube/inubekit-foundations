e4cb7f26644662cd2d730a69c5a27bfc15f1ab35
Januar Asprilla <ajanuarandres@gmail.com>
2024-01-23 23:24:34 -0500

    feat: exporting tokens in the inube object

---
+++ src/tokens/index.ts
@@ -0,0 +1,11 @@
+import { color } from "./colors";
+import { spacing } from "./spacing";
+import { typography } from "./typography";
+
+const inube = {
+  color,
+  spacing,
+  typography,
+};
+
+export { inube };
