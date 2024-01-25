541dfebd673d2858446db623a94c05476471d30b
Januar Asprilla <ajanuarandres@gmail.com>
2024-01-25 00:12:19 -0500

    feat: creation of the present theme, in addition to its import

---
+++ src/shared/themes/presente/colors/index.ts
@@ -0,0 +1,13 @@
+import { palette } from "./palette";
+import { text } from "./text";
+import { stroke } from "./stroke";
+import { surface } from "./surface";
+
+const color = {
+  palette,
+  text,
+  stroke,
+  surface,
+};
+
+export { color };
