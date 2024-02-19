11b2676479828427d0fbb972e4ba021182fa0c55
cmarin001 <cmarin6051@gmail.com>
2024-02-19 12:14:24 -0500

    fix(spinner): added spinner-tokens to main inube export file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,13 @@
 import { palette } from "./palette";
 import { text } from "./text";
 import { icon } from "./icon";
+import { spinner } from "./spinner";
 
 const inube = {
   palette,
   text,
   icon,
+  spinner,
 };
 
 export { inube };
