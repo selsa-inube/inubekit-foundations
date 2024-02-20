bcab849326de37185c4314b34b620ba3d9473da6
cmarin001 <cmarin6051@gmail.com>
2024-02-20 16:01:37 -0500

    fix(button): added tokens-file to main-exportation file

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,10 @@ import { text } from "./text";
 import { icon } from "./icon";
 import { fieldset } from "./fieldset";
 import { spinner } from "./spinner";
+import { button } from "./button";
 
 const inube = {
+  button,
   icon,
   fieldset,
   palette,
