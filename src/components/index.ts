a6db4930bbd829b1f03fdf5aaa473c8e29e0de6f
cmarin001 <cmarin6051@gmail.com>
2024-02-20 14:56:55 -0500

    fix(switch): adjust tokens according to the new requirements

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,12 +3,14 @@ import { text } from "./text";
 import { icon } from "./icon";
 import { fieldset } from "./fieldset";
 import { spinner } from "./spinner";
+import { Switch } from "./switch";
 
 const inube = {
   icon,
   fieldset,
   palette,
   spinner,
+  Switch,
   text,
 };
 
---
+++ src/components/switch.ts
@@ -0,0 +1,58 @@
+import { palette } from "./palette";
+
+const Switch = {
+  on: {
+    background: {
+      color: {
+        regular: palette.green.G400,
+        disabled: palette.neutral.N20,
+        hover: palette.green.G300,
+      },
+    },
+    toggleBackground: {
+      color: {
+        regular: palette.neutral.N0,
+        disabled: palette.neutral.N0,
+        hover: palette.neutral.N0,
+      },
+    },
+    toggleBorder: {
+      color: {
+        regular: palette.neutralAlpha.N0A,
+        disabled: palette.neutral.N70,
+        hover: palette.neutralAlpha.N0A,
+      },
+    },
+    icon: {
+      appereance: "light",
+    },
+  },
+  off: {
+    background: {
+      color: {
+        regular: palette.neutral.N20,
+        disabled: palette.neutral.N20,
+        hover: palette.neutral.N10,
+      },
+    },
+    toggleBackground: {
+      color: {
+        regular: palette.neutral.N0,
+        disabled: palette.neutral.N0,
+        hover: palette.neutral.N0,
+      },
+    },
+    toggleBorder: {
+      color: {
+        regular: palette.neutral.N70,
+        disabled: palette.neutral.N70,
+        hover: palette.neutral.N70,
+      },
+    },
+    icon: {
+      appereance: "gray",
+    },
+  },
+};
+
+export { Switch };
