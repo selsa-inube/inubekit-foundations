51a7ea9dd800d9c1c2e966b9639327b15dca4a84
Januar Asprilla <ajanuarandres@gmail.com>
2024-02-20 13:52:11 -0500

    feat: new label tokens created

---
+++ src/components/label.ts
@@ -0,0 +1,12 @@
+import { palette } from "./palette";
+
+export const label = {
+  content: {
+    color: {
+      regular: palette.neutral.N900,
+      disabled: palette.neutral.N70,
+      focus: palette.blue.B300,
+      invalid: palette.red.R300,
+    },
+  },
+};
