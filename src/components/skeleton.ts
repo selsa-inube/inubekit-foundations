562e7233834d9952b0cc3c279d431cb7ad0640e8
Januar Asprilla <ajanuarandres@gmail.com>
2024-03-05 17:48:58 -0500

    feat: tokens are created and exported in Skeleton

---
+++ src/components/skeleton.ts
@@ -0,0 +1,12 @@
+import { palette } from "./palette";
+
+const skeleton = {
+  background: {
+    color: palette.neutral.N30,
+  },
+  animation: {
+    color: palette.neutral.N10,
+  },
+};
+
+export { skeleton };
