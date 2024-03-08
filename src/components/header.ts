72ffcb963a0d7957a961a7ceab41233c4dced3f1
Januar Asprilla <ajanuarandres@gmail.com>
2024-03-08 17:24:49 -0500

    feat: tokens are created and exported in tabs, breadcrumbs, blanket, header

---
+++ src/components/header.ts
@@ -0,0 +1,12 @@
+import { palette } from "./palette";
+
+const header = {
+  background: {
+    color: palette.neutral.N0,
+  },
+  content: {
+    appearance: "gray",
+  },
+};
+
+export { header };
