f5a1c1af5e217cc07c45743fe8b02ac7424eff21
Januar Asprilla <ajanuarandres@gmail.com>
2024-03-13 20:33:04 -0500

    feat: tokens created the link

---
+++ src/components/link.ts
@@ -0,0 +1,17 @@
+import { palette } from "./palette";
+
+const link = {
+  content: {
+    color: {
+      regular: palette.blue.B400,
+      hover: palette.blue.B300,
+    },
+  },
+  underline: {
+    color: {
+      hover: palette.blue.B300,
+    },
+  },
+};
+
+export { link };
